39c221ea620bfd216b786a5f5c410b9173b71650
sese-bit <111631857+sese-bit@users.noreply.github.com>
2022-11-05 14:28:29 +0800

    Add files via upload

---
+++ midterms/midterms/peysapp/act2-peys-app.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>paysapp</title>
+</head>
+<body>
+    <h1>Peys App</h1>
+    <form  method ="get">
+    <label for="imgSize">Select Photo Size:</label>
+     <input type="range" id="imgSize" name="imgSize"  min="10" max="100" step="10" value="60"><br>
+    
+    <label for="bColor">Select Border Color:</label>
+    <input type="color" name="bdColor" id="bdColor"><br>
+    
+    <input type="submit" name="btnSave" value="Process"><br>
+    
+    <?php
+    $imageRangeSize = 60;
+    if(isset($_GET['btnSave'])){
+        
+        $imageRangeSize = $_GET['imgSize'];
+        $border = $_GET['bdColor'];
+    }
+    ?>
+    <br>
+  <img src="xxx.jpg" style="width: <?php echo $imageRangeSize . '%'; ?>;border-color: <?php echo $border; ?>;border-width: 5px; border-style: solid;">
+
+    </form>
+
+</body>
+</html>
